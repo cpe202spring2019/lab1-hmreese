44eb9a2823dd22af0e94f5aa11e1b844076d0959
Hannah Reese <hreese@calpoly.edu>
2019-04-10 14:58:52 -0700

    initial commit of test cases

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -1,25 +1,40 @@
-aimport unittest
+# Lab 1 Test Cases
+
+import unittest
 from lab1 import *
 
- # A few test cases.  Add more!!!
 class TestLab1(unittest.TestCase):
 
     def test_max_list_iter(self):
-        """add description here"""
+        tlist = None
+        with self.assertRaises(ValueError): # used to check for exception
+            max_list_iter(tlist)
+        test_list = [1, 2, 3]
+        self.assertEqual(max_list_iter(test_list), 3) # check for correct return
+        empty_list = []
+        self.assertEqual(max_list_iter(empty_list), None) # check for empty list
+
+
+    def test_reverse_rec(self):
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
+        self.assertEqual(reverse_rec([1, 2, 3]), [3, 2, 1]) # check for correct return
+        self.assertEqual(reverse_rec([3, 1, 2]), [2, 1, 3]) # check non-ordered list
+        self.assertEqual(reverse_rec([4, 1, 9, 0]), [0, 9, 1, 4]) # check longer non-ordered list
+        self.assertEqual(reverse_rec([]), None) # check for empty list
 
-    def test_reverse_rec(self):
-        self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
 
     def test_bin_search(self):
-        list_val =[0,1,2,3,4,7,8,9,10]
+        list_val =[0, 1, 2, 3, 4, 7, 8, 9, 10]
         low = 0
-        high = len(list_val)-1
-        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+        high = len(list_val) - 1
+        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4) # target < mid
+        self.assertEqual(bin_search(-1, low, high, list_val), None) # lower target not in list
+        self.assertEqual(bin_search(9, low, high, list_val), 7) # target > mid
+        self.assertEqual(bin_search(11, low, high, list_val), None) # higher target not in list
+
 
 if __name__ == "__main__":
         unittest.main()
 
-    
