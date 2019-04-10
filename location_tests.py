7d7fb12fb47ea3105a9501c1983434bcdff5c9a1
hreese <hreese@calpoly.edu>
2019-04-10 14:44:20 -0700

    Initial Commit

--- location_tests.py
+++ location_tests.py
@@ -1,5 +1,10 @@
+# CPE 202
+# Lab 0 Tests
+
+
 import unittest
 from location import *
+
 
 class TestLab1(unittest.TestCase):
 
@@ -7,7 +12,11 @@ class TestLab1(unittest.TestCase):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
     
-    # Add more tests!
+    def test_eq(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("SLO", 35.3, -120.7)
+        self.assertEqual(loc1, loc2)
 
+    
 if __name__ == "__main__":
         unittest.main()
